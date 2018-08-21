efbabe96c0d8a732a1da7dbb0ce2b3ad273faabe
yeswanth12 <42166121+yeswanth12@users.noreply.github.com>
2018-08-21 23:07:58 +0530

    Update redundant_braces.cpp

--- redundant_braces.cpp
+++ redundant_braces.cpp
@@ -1,3 +1,11 @@
+/*Write a program to validate if the input string has redundant braces?
+Return 0/1
+
+0 -> NO
+1 -> YES*/
+
+---------------------****-------------------------------
+
 int Solution::braces(string a) {
    
     // stack which contains the
